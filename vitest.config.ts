b2931b6708de2a9ed4b51509a0007380e9f6ab90
Alex Berriman <alex.berriman@live.com>
2025-04-08 15:59:46 +1000

    feat: prettier and eslint

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    // Vitest configuration options
+    globals: true,
+    environment: "node",
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: ["**/node_modules/**", "tests/**"],
+    },
+    mockReset: true,
+    restoreMocks: true,
   },
 });
